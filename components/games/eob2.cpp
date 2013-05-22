269e499c067abee38e1b1a0dbbc8165121e92a20
Bret Curtis <psi29a@gmail.com>
2013-05-22 09:58:58 +0200

    added eob2 module that will handle things exclusively eob2 related

---
+++ components/games/eob2.cpp
@@ -0,0 +1,16 @@
+#include "eob2.hpp"
+
+namespace Games
+{
+
+void gameInit(void *palette, int from, int length)
+{
+	return;
+}
+
+void loadImages(void *palette, int from, int length)
+{
+	return;
+}
+
+}
---
+++ components/games/eob2.hpp
@@ -0,0 +1,10 @@
+#ifndef COMPONENTS_GAME_EOB2_HPP
+#define COMPONENTS_GAME_EOB2_HPP
+
+namespace Games
+{
+	bool gameInit(void);
+	bool loadImages(void);
+}
+
+#endif /* COMPONENTS_GAME_EOB2_HPP */
