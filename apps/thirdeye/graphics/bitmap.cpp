4e4e30aad8cdd69b6f3773690af42a43e5a4ea34
Bret Curtis <psi29a@gmail.com>
2013-08-26 21:45:30 +0200

    wip: push more code to bitmap handler

--- apps/thirdeye/graphics/bitmap.cpp
+++ apps/thirdeye/graphics/bitmap.cpp
@@ -8,6 +8,19 @@ uint16_t GRAPHICS::Bitmap::getFilesize() const {
 	return *reinterpret_cast<const uint16_t*>(&vec_[0]);
 }
 
+uint16_t GRAPHICS::Bitmap::getNumberOfBitmaps() const {
+	return *reinterpret_cast<const uint16_t*>(&vec_[2 * 2]);
+}
+
+std::map<uint16_t, uint32_t> GRAPHICS::Bitmap::getBitmapOffsets() const {
+	std::map<uint16_t, uint32_t> offsets;
+	for (uint16_t i = 0; i < getNumberOfBitmaps(); i++) {
+		offsets[i] = *reinterpret_cast<const uint16_t*>(&vec_[6 + i * 4]);
+	}
+
+	return offsets;
+}
+
 uint16_t GRAPHICS::Bitmap::getWidth() const {
 	return *reinterpret_cast<const uint16_t*>(&vec_[5 * 2]);
 }
@@ -16,6 +29,8 @@ uint16_t GRAPHICS::Bitmap::getHeight() const {
 	return *reinterpret_cast<const uint16_t*>(&vec_[6 * 2]);
 }
 
+
+
 uint8_t& GRAPHICS::Bitmap::operator[](size_t off) {
 	if (off > vec_.size() - BMPHEADEROFFSET) {
 		std::cerr << "Trying to access BMP data out of bounds." << std::endl;
@@ -23,4 +38,6 @@ uint8_t& GRAPHICS::Bitmap::operator[](size_t off) {
 	}
 //std::cout << "offset @: " << off << std::endl;
 	return vec_[off + BMPHEADEROFFSET];
+
+
 }
