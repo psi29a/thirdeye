f1a8c2effcea79a21175fb4ad47b24ddbd96a924
Bret Curtis <psi29a@gmail.com>
2013-09-29 10:59:35 +0200

    we can now dig deeper, but still need to make a loop... or even recursively handle it

--- apps/thirdeye/resources/gffi.cpp
+++ apps/thirdeye/resources/gffi.cpp
@@ -68,13 +68,14 @@ RESOURCES::GFFI::GFFI(boost::filesystem::path gffiPath) {
 			block++) {
 
 		GFFIBlockHeader mGFFIBlockHeader;
-		fResource.read(reinterpret_cast<char*>(&mGFFIBlockHeader), sizeof(GFFIBlockHeader));
+		fResource.read(reinterpret_cast<char*>(&mGFFIBlockHeader),
+				sizeof(GFFIBlockHeader));
 
 		//if (mGFFIBlockHeader.tag[3] == 0x20)
 		mGFFIBlockHeader.tag[3] = '\0';
 
-		for (uint16_t elements = 0; elements < mGFFIBlockHeader.number_of_elements;
-				elements++) {
+		for (uint16_t elements = 0;
+				elements < mGFFIBlockHeader.number_of_elements; elements++) {
 
 			GFFIBlock mGFFIBlock;
 
@@ -84,13 +85,14 @@ RESOURCES::GFFI::GFFI(boost::filesystem::path gffiPath) {
 			std::string tag = mGFFIBlockHeader.tag;
 			//tag[3] =  '\0';	// needs null, but we loose last char
 
-			if (tag == "BMA"){
-			std::cout << std::hex << "    tag: " << tag
-					<< std::endl << "    elements: "
-					<< mGFFIBlockHeader.number_of_elements << std::endl
-					<< "    unique: " << mGFFIBlock.unique << std::endl
-					<< "    offset: " << mGFFIBlock.offset << std::endl
-					<< "    size: " << mGFFIBlock.size << std::endl << std::endl;
+			if (tag == "BMA") {
+				std::cout << std::hex << "    tag: " << tag << std::endl
+						<< "    elements: "
+						<< mGFFIBlockHeader.number_of_elements << std::endl
+						<< "    unique: " << mGFFIBlock.unique << std::endl
+						<< "    offset: " << mGFFIBlock.offset << std::endl
+						<< "    size: " << mGFFIBlock.size << std::endl
+						<< std::endl;
 			}
 
 			mFiles[tag][mGFFIBlock.unique].offset = mGFFIBlock.offset;
@@ -101,19 +103,28 @@ RESOURCES::GFFI::GFFI(boost::filesystem::path gffiPath) {
 
 	// Now we dump the data
 	//std::cout << "Data dump:" << std::endl;
-	std::map< std::string, std::map< uint32_t, File > >::iterator tag;
+	std::map<std::string, std::map<uint32_t, File> >::iterator tag;
 	for (tag = mFiles.begin(); tag != mFiles.end(); tag++) {
-		std::map< uint32_t, File >::iterator file;
+		std::map<uint32_t, File>::iterator file;
 		for (file = tag->second.begin(); file != tag->second.end(); file++) {
 
 			seek(fResource, file->second.offset, BOOST_IOS::beg);
 			fResource.read(reinterpret_cast<char*>(&file->second.data[0]),
 					file->second.data.size());
 
-			if (tag->first == "BMA"){
+			if (tag->first == "BMA") {
 				GRAPHICS::Bitmap anim(file->second.data);
 				anim[anim.getNumberOfBitmaps()-1];
 				printf("Is more: %d @ offset: %d\n", anim.isMoreBitmap(), anim.getNextBitmapPos());
+				if (anim.isMoreBitmap()){
+					std::vector<uint8_t> subBitmap(file->second.data.size()-anim.getNextBitmapPos());
+					memcpy(&subBitmap[0],&file->second.data[0] + anim.getNextBitmapPos(), file->second.data.size()-anim.getNextBitmapPos());
+					//std::copy(file->second.data[anim.getNextBitmapPos()] ,file->second.data[file->second.data.size()], subBitmap.begin());
+					GRAPHICS::Bitmap subAnim(subBitmap);
+					subAnim[subAnim.getNumberOfBitmaps()-1];
+					printf("  Sub is more: %d @ offset: %d\n", anim.isMoreBitmap(), anim.getNextBitmapPos());
+				}
+
 			}
 			/*
 			std::string Path = "/tmp/"+boost::lexical_cast<std::string>(file->first)+"."+tag->first;
@@ -128,15 +139,15 @@ RESOURCES::GFFI::GFFI(boost::filesystem::path gffiPath) {
 			*/
 		}
 
+		}
+		//showResources();
 	}
-	//showResources();
-}
 
 RESOURCES::GFFI::~GFFI() {
 	//cleanup
 }
 
-std::map<uint8_t, tuple<uint8_t, uint8_t, std::vector<uint8_t> > > RESOURCES::GFFI::getSequence(){
+std::map<uint8_t, tuple<uint8_t, uint8_t, std::vector<uint8_t> > > RESOURCES::GFFI::getSequence() {
 	std::map<uint8_t, tuple<uint8_t, uint8_t, std::vector<uint8_t> > > sequences;
 	sequences[0] = boost::make_tuple(SETT_PAL, 0, mFiles["PAL"][1].data);
 	sequences[1] = boost::make_tuple(FADE_IN, 5, mFiles["BMP"][1].data);
