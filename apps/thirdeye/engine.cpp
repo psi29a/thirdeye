3bffd9a202f36a28d430cf1c1923b76882a1a4c8
Bret Curtis <psi29a@gmail.com>
2015-08-24 10:20:04 +0200

    decompiling start script

--- apps/thirdeye/engine.cpp
+++ apps/thirdeye/engine.cpp
@@ -1,8 +1,10 @@
+#include <fstream>  // TODO: remove when finished
 #include "engine.hpp"
 #include "resources/res.hpp"
 #include "resources/gffi.hpp"
 #include "sound/sound.hpp"
 #include "graphics/graphics.hpp"
+#include "state/aesop.hpp"
 
 #include <components/files/configurationmanager.hpp>
 
@@ -10,13 +12,13 @@
 using boost::algorithm::to_lower;
 
 THIRDEYE::Engine::Engine(Files::ConfigurationManager& configurationManager) :
-		mNewGame(false), mUseSound(true), mDebug(false), mGame(GAME_UNKN), mCfgMgr(
-				configurationManager), mScale(1), mRenderer(false) {
-	std::cout << "Initializing Thirdeye... ";
+        mNewGame(false), mUseSound(true), mDebug(false), mGame(GAME_UNKN), mCfgMgr(
+                configurationManager), mScale(1), mRenderer(false) {
+    std::cout << "Initializing Thirdeye... ";
 
-	std::srand(std::time(NULL));
+    std::srand(std::time(NULL));
 
-	std::cout << "done!" << std::endl;
+    std::cout << "done!" << std::endl;
 }
 
 THIRDEYE::Engine::~Engine() {
@@ -25,208 +27,222 @@ THIRDEYE::Engine::~Engine() {
 
 // Setup engine via parameters
 void THIRDEYE::Engine::setGame(std::string game) {
-	boost::algorithm::to_lower(game);
+    boost::algorithm::to_lower(game);
 
-	if (game == "eob3") {
-		mGame = GAME_EOB3;
-		mGameData /= "EYE.RES";
-	} else if (game == "hack") {
-		mGame = GAME_HACK;
-		mGameData /= "HACK.RES";
-	} else
-		mGame = GAME_UNKN;
+    if (game == "eob3") {
+        mGame = GAME_EOB3;
+        mGameData /= "EYE.RES";
+    } else if (game == "hack") {
+        mGame = GAME_HACK;
+        mGameData /= "HACK.RES";
+    } else
+        mGame = GAME_UNKN;
 
 }
 
 void THIRDEYE::Engine::setGameData(std::string gameData) {
-	mGameData = boost::filesystem::path(gameData);
+    mGameData = boost::filesystem::path(gameData);
 }
 void THIRDEYE::Engine::setDebugMode(bool debug) {
-	mDebug = debug;
+    mDebug = debug;
 }
 void THIRDEYE::Engine::setSoundUsage(bool nosound) {
-	mUseSound = !nosound;
+    mUseSound = !nosound;
 }
 void THIRDEYE::Engine::setScale(uint16_t scale) {
-	mScale = scale;
+    mScale = scale;
 }
 void THIRDEYE::Engine::setRenderer(bool renderer) {
-	mRenderer = renderer;
+    mRenderer = renderer;
 }
 
 // Initialise and enter main loop.
 void THIRDEYE::Engine::go() {
-	MIXER::Mixer mixer;		// setup our sound mixer
-	GRAPHICS::Graphics gfx(mScale); // setup our graphics
-	RESOURCES::Resource resource(mGameData);	// get our game resources ready
+    MIXER::Mixer mixer;		// setup our sound mixer
+    GRAPHICS::Graphics gfx(mScale); // setup our graphics
+    RESOURCES::Resource resource(mGameData);	// get our game resources ready
 
-	/*
-	 Settings::Manager settings;
-	 std::string settingspath;
+    /*
+     Settings::Manager settings;
+     std::string settingspath;
 
-	 settingspath = loadSettings (settings);
-	 */
+     settingspath = loadSettings (settings);
+     */
 
-	std::vector<uint8_t> &snd = resource.getAsset("BIRD4");
-	std::vector<uint8_t> &xmidi = resource.getAsset("CUE1");
+    std::vector<uint8_t> &snd = resource.getAsset("BIRD4");
+    std::vector<uint8_t> &xmidi = resource.getAsset("CUE1");
 
-	//std::vector<uint8_t> &font = resource.getAsset("8x8 font");
-	//std::vector<uint8_t> &font2 = resource.getAsset("6x8 font");
-	//std::vector<uint8_t> &font3 = resource.getAsset("Ornate font");
+    //std::vector<uint8_t> &font = resource.getAsset("8x8 font");
+    //std::vector<uint8_t> &font2 = resource.getAsset("6x8 font");
+    //std::vector<uint8_t> &font3 = resource.getAsset("Ornate font");
 
-	//std::vector<uint8_t> &bmp = resource.getAsset("Backdrop");
-	//std::vector<uint8_t> &bmp = resource.getAsset("Menu shapes");
-	std::vector<uint8_t> &icons = resource.getAsset("Icons");
-	//std::vector<uint8_t> &marble = resource.getAsset("Marble walls");
-	//std::vector<uint8_t> &basePalette = resource.getAsset("Fixed palette");
-	std::vector<uint8_t> &basePalette = resource.getAsset("Title palette");
-	//std::vector<uint8_t> &subPalette = resource.getAsset("Marble palette");
-	std::string text = resource.getTableEntry("Marble palette", 1);
+    //std::vector<uint8_t> &bmp = resource.getAsset("Backdrop");
+    //std::vector<uint8_t> &bmp = resource.getAsset("Menu shapes");
+    std::vector<uint8_t> &icons = resource.getAsset("Icons");
+    //std::vector<uint8_t> &marble = resource.getAsset("Marble walls");
+    //std::vector<uint8_t> &basePalette = resource.getAsset("Fixed palette");
+    std::vector<uint8_t> &basePalette = resource.getAsset("Title palette");
+    //std::vector<uint8_t> &subPalette = resource.getAsset("Marble palette");
+    std::string text = resource.getTableEntry("Marble palette", 1);
 
-	gfx.loadPalette(basePalette);
-	gfx.loadMouse(icons, 0);
+    gfx.loadPalette(basePalette);
+    gfx.loadMouse(icons, 0);
 
-	//gfx.drawImage(bmp, 0, 0, 0, true);
-
-
-	 /*
-	 gfx.loadPalette(basePalette, subPalette, text);
-	 gfx.drawImage(marble, 18, 0, 0, true);
-	 gfx.drawImage(marble, 0, 0, 0, true);
-	 gfx.drawImage(marble, 1, 24, 8, true);
-	 gfx.drawImage(marble, 2, 48, 20, true);
-	 gfx.drawImage(marble, 3, 64, 28, true);
-
-	 gfx.drawImage(icons, 1, 25, 120, true);
-	 gfx.drawText(font,"Welcome to Thirdeye!", 8, 181);
-	 */
-
-	uint32_t clock = 0;	//  wall clock in ms resolution
-	uint32_t currentSecond = 0;	// our wall clock with 1s resolution
-	uint32_t fps = 0;	// number of fps (iterations of main loop)
-
-	// get our intro cinematic, set state and play
-	RESOURCES::GFFI introVideo(mGameData.remove_leaf() /= "INTRO.GFF");
-	mixer.playMusic(introVideo.getMusic());
-	gfx.playVideo(introVideo.getSequence());
-	uint8_t state = STATE_INTRO;
-
-	// Start the main rendering loop
-	SDL_Event event;
-	bool done = false;
-	bool isSecond = false;
-	while (!done)  // Enter main loop.
-	{
-		// get our clock
-		clock = SDL_GetTicks();
-		fps++;
-		if (clock / 1000 > currentSecond) {
-			currentSecond = clock / 1000;
-			//update = true;
-			isSecond = true;
-		}
-
-		// what state are we in
-		switch (state) {
-			case STATE_INTRO: // change state to menu when finished playing intro
-				if ( !gfx.isVideoPlaying() ){
-					state = STATE_MENU;
-					std::vector<uint8_t> &menuBMP = resource.getAsset("Menu shapes");
-					std::vector<uint8_t> &menuPAL = resource.getAsset("Title palette");
-					gfx.loadPalette(menuPAL);
-					gfx.zoomIntoImage(menuBMP);
-				}
-
-		}
-
-		// poll our inputs
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			// this is the window x being clicked.
-			case SDL_QUIT:
-				done = true;
-				break;
-				// process the mouse data by passing it to ngl class
-			case SDL_MOUSEMOTION:
-				//ngl.mouseMoveEvent(event.motion);
-				//std::cout << "Mouse moved @ " <<  event.motion.x << " " << event.motion.y << std::endl;
-				break;
-			case SDL_MOUSEBUTTONDOWN:
-				break;
-			case SDL_MOUSEBUTTONUP:
-				//std::cout << "Mouse clicked @ " << event.button.x << " " << event.button.y << std::endl;
-				break;
-				//case SDL_MOUSEWHEEL : ngl.wheelEvent(event.wheel);
-				// if the window is re-sized pass it to the ngl class to change gl viewport
-				// note this is slow as the context is re-create by SDL each time
-			case SDL_WINDOWEVENT:
-				//int w, h;
-				// get the new window size
-				//SDL_GetWindowSize(window, &w, &h);
-				//ngl.resize(w,h);
-				break;
-
-				// now we look for a keydown event
-			case SDL_KEYDOWN: {
-				switch (event.key.keysym.sym) {
-				// if it's the escape key quit
-				case SDLK_ESCAPE:
-					if ( state == STATE_INTRO ){
-						gfx.stopVideo();
-						mixer.stopMusic();
-					} else
-						done = true;
-					break;
-				case SDLK_w:
-					mixer.playSound(snd);
-					break;
-				case SDLK_a:
-					break;
-				case SDLK_s:
-					mixer.playMusic(xmidi);
-					break;
-				case SDLK_d:
-					break;
-				case SDLK_f:
-					//SDL_SetWindowFullscreen(window, SDL_TRUE);
-					break;
-				case SDLK_g:
-					//SDL_SetWindowFullscreen(window, SDL_FALSE);
-					break;
-				default:
-					break;
-				} // end of key process
-			}
-				break; // end of keydown
-
-			default:
-				break;
-			} // end of event switch
-		} // end pool loop
+    //gfx.drawImage(bmp, 0, 0, 0, true);
 
 
-		gfx.update();		// update our screen
-		mixer.update();		// update our sounds
+     /*
+     gfx.loadPalette(basePalette, subPalette, text);
+     gfx.drawImage(marble, 18, 0, 0, true);
+     gfx.drawImage(marble, 0, 0, 0, true);
+     gfx.drawImage(marble, 1, 24, 8, true);
+     gfx.drawImage(marble, 2, 48, 20, true);
+     gfx.drawImage(marble, 3, 64, 28, true);
 
-		if (isSecond) {
-			std::cout << "Wall Clock: " << std::dec << currentSecond << " average " << fps << "fps"
-					//<< " sleeping for: " << (int) gfx.getSleep() << "ms "
-					<< "\r" << std::flush;
-			isSecond = false;
-			fps = 0;
-		}
+     gfx.drawImage(icons, 1, 25, 120, true);
+     gfx.drawText(font,"Welcome to Thirdeye!", 8, 181);
+     */
 
-		uint32_t sleep = gfx.getSleep();
-		if ( sleep > 0 )
-			SDL_Delay(gfx.getSleep());
-		else {
-			SDL_Delay(fps/5);
-		}
-	}
+    uint32_t clock = 0;	//  wall clock in ms resolution
+    uint32_t currentSecond = 0;	// our wall clock with 1s resolution
+    uint32_t fps = 0;	// number of fps (iterations of main loop)
 
-	// Save user settings
-	//settings.saveUser(settingspath);
+    // get our intro cinematic, set state and play
+    RESOURCES::GFFI introVideo(mGameData.remove_leaf() /= "INTRO.GFF");
+    mixer.playMusic(introVideo.getMusic());
+    gfx.playVideo(introVideo.getSequence());
+    uint8_t state = STATE_INTRO;
 
-	std::cout << "Quitting peacefully." << std::endl;
+    // Start the main rendering loop
+    SDL_Event event;
+    bool done = false;
+    bool isSecond = false;
+
+    /* TODO: remove when done with SOP */
+    done = true;
+    std::vector<uint8_t> &sop = resource.getAsset("start");
+    AESOP::Aesop state_engine(sop);
+    state_engine.show();
+
+    /*
+    std::string Path = "./start.EXPT";
+    std::ofstream FILE(Path.c_str(), std::ios::out | std::ofstream::binary);
+    size_t sz = sop.size();
+    FILE.write(reinterpret_cast<const char*>(&sop[0]), sz * sizeof(sop[0]));
+    */
+
+    while (!done)  // Enter main loop.
+    {
+        // get our clock
+        clock = SDL_GetTicks();
+        fps++;
+        if (clock / 1000 > currentSecond) {
+            currentSecond = clock / 1000;
+            //update = true;
+            isSecond = true;
+        }
+
+        // what state are we in
+        switch (state) {
+            case STATE_INTRO: // change state to menu when finished playing intro
+                if ( !gfx.isVideoPlaying() ){
+                    state = STATE_MENU;
+                    std::vector<uint8_t> &menuBMP = resource.getAsset("Menu shapes");
+                    std::vector<uint8_t> &menuPAL = resource.getAsset("Title palette");
+                    gfx.loadPalette(menuPAL);
+                    gfx.zoomIntoImage(menuBMP);
+                }
+
+        }
+
+        // poll our inputs
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            // this is the window x being clicked.
+            case SDL_QUIT:
+                done = true;
+                break;
+                // process the mouse data by passing it to ngl class
+            case SDL_MOUSEMOTION:
+                //ngl.mouseMoveEvent(event.motion);
+                //std::cout << "Mouse moved @ " <<  event.motion.x << " " << event.motion.y << std::endl;
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                break;
+            case SDL_MOUSEBUTTONUP:
+                //std::cout << "Mouse clicked @ " << event.button.x << " " << event.button.y << std::endl;
+                break;
+                //case SDL_MOUSEWHEEL : ngl.wheelEvent(event.wheel);
+                // if the window is re-sized pass it to the ngl class to change gl viewport
+                // note this is slow as the context is re-create by SDL each time
+            case SDL_WINDOWEVENT:
+                //int w, h;
+                // get the new window size
+                //SDL_GetWindowSize(window, &w, &h);
+                //ngl.resize(w,h);
+                break;
+
+                // now we look for a keydown event
+            case SDL_KEYDOWN: {
+                switch (event.key.keysym.sym) {
+                // if it's the escape key quit
+                case SDLK_ESCAPE:
+                    if ( state == STATE_INTRO ){
+                        gfx.stopVideo();
+                        mixer.stopMusic();
+                    } else
+                        done = true;
+                    break;
+                case SDLK_w:
+                    mixer.playSound(snd);
+                    break;
+                case SDLK_a:
+                    break;
+                case SDLK_s:
+                    mixer.playMusic(xmidi);
+                    break;
+                case SDLK_d:
+                    break;
+                case SDLK_f:
+                    //SDL_SetWindowFullscreen(window, SDL_TRUE);
+                    break;
+                case SDLK_g:
+                    //SDL_SetWindowFullscreen(window, SDL_FALSE);
+                    break;
+                default:
+                    break;
+                } // end of key process
+            }
+                break; // end of keydown
+
+            default:
+                break;
+            } // end of event switch
+        } // end pool loop
+
+
+        gfx.update();		// update our screen
+        mixer.update();		// update our sounds
+
+        if (isSecond) {
+            std::cout << "Wall Clock: " << std::dec << currentSecond << " average " << fps << "fps"
+                    //<< " sleeping for: " << (int) gfx.getSleep() << "ms "
+                    << "\r" << std::flush;
+            isSecond = false;
+            fps = 0;
+        }
+
+        uint32_t sleep = gfx.getSleep();
+        if ( sleep > 0 )
+            SDL_Delay(gfx.getSleep());
+        else {
+            SDL_Delay(fps/5);
+        }
+    }
+
+    // Save user settings
+    //settings.saveUser(settingspath);
+
+    std::cout << "Quitting peacefully." << std::endl;
 }
 
---
+++ apps/thirdeye/state/aesop.hpp
@@ -0,0 +1,141 @@
+/*
+ * aesop.hpp
+ *
+ *  Created on: Aug 19, 2015
+ *      Author: bcurtis
+ */
+
+#include <stdint.h>
+#include <vector>
+
+#ifndef AESOP_HPP
+#define AESOP_HPP
+
+namespace AESOP {
+
+#define OP_BRT  0x00    /* 00   BRT   word            BRanch if True   */
+#define OP_BRF  0x01    /* 01   BRF   word            BRanch if False  */
+#define OP_BRA  0x02    /* 02   BRA   word            BRanch Always    */
+#define OP_CASE 0x03    /* 03   CASE  (see below)     CASE selection   */
+#define OP_PUSH 0x04    /* 04   PUSH  -               PUSH 0 onto stack (used as a parameter delimiter for CALL, SEND) */
+#define OP_DUP  0x05    /* 05   DUP   -               DUPlicate top of stack    */
+#define OP_NOT  0x06    /* 06   NOT   -               Logical NOT (unary)       */
+#define OP_SETB 0x07    /* 07   SETB  -               SET Boolean value (unary) */
+#define OP_NEG  0x08    /* 08   NEG   -               NEGate (unary)    */
+#define OP_ADD  0x09    /* 09   ADD   -               ADD (binary)      */
+#define OP_SUB  0x0A    /* 0A   SUB   -               SUBtract (binary) */
+#define OP_MUL  0x0B    /* 0B   MUL   -               MULtiply (binary) */
+#define OP_DIV  0x0C    /* 0C   DIV   -               DIVide (binary)   */
+#define OP_MOD  0x0D    /* 0D   MOD   -               MODulus (binary)  */
+#define OP_EXP  0x0E    /* 0E   EXP   -               EXPonent (binary) */
+#define OP_BAND 0x0F    /* 0F   BAND  -               Bitwise AND (binary)      */
+#define OP_BOR  0x10    /* 10   BOR   -               Bitwise OR (binary)       */
+#define OP_XOR  0x11    /* 11   XOR   -               Bitwise XOR (binary)      */
+#define OP_BNOT 0x12    /* 12   BNOT  -               Bitwise NOT (unary)       */
+#define OP_SHL  0x13    /* 13   SHL   -               SHift Left (binary)       */
+#define OP_SHR  0x14    /* 14   SHR   -               SHift Right (binary)      */
+#define OP_LT   0x15    /* 15   LT    -               Less Than (binary)        */
+#define OP_LE   0x16    /* 16   LE    -               Less than or Equal (binary)   */
+#define OP_EQ   0x17    /* 17   EQ    -               EQual (binary)    */
+#define OP_NE   0x18    /* 18   NE    -               Not Equal (binary)        */
+#define OP_GE   0x19    /* 19   GE    -               Greater than or Equal (binary)    */
+#define OP_GT   0x1A    /* 1A   GT    -               Greather Than (binary)    */
+#define OP_INC  0x1B    /* 1B   INC   -               INCrement (unary) */
+#define OP_DEC  0x1C    /* 1C   DEC   -               DECrement (unary) */
+#define OP_SHTC 0x1D    /* 1D   SHTC  byte            Load SHorT Constant (0...256) */
+#define OP_INTC 0x1E    /* 1E   INTC  word            Load INTeger Constant (256...64K) */
+#define OP_LNGC 0x1F    /* 1F   LNGC  long            Load LoNG Constant (64K...4G) */
+#define OP_RCRS 0x20    /* 20   RCRS  word            Reference Code ReSource address (auxiliary for CALL)  */
+#define OP_CALL 0x21    /* 21   CALL  byte            CALL code resource handler  (call of the runtime function)    */
+#define OP_SEND 0x22    /* 22   SEND  byte, word      SEND message (sending message to another object - it in fact calls an object method)  */
+#define OP_PASS 0x23    /* 23   PASS  byte            PASS message to parent class */
+#define OP_JSR  0x24    /* 24   JSR   word            Jump to SubRoutine        */
+#define OP_RTS  0x25    /* 25   RTS   -               ReTurn from Subroutine    */
+#define OP_AIM  0x26    /* 26   AIM   word            Array Index Multiply      */
+#define OP_AIS  0x27    /* 27   AIS   byte            Array Index Shift         */
+#define OP_LTBA 0x28    /* 28   LTBA  word            Load Table Byte Array     */
+#define OP_LTWA 0x29    /* 29   LTWA  word            Load Table Word Array     */
+#define OP_LTDA 0x2A    /* 2A   LTDA  word            Load Table Dword Array    */
+#define OP_LETA 0x2B    /* 2B   LETA  word            Load Effective Table Address  */
+#define OP_LAB  0x2C    /* 2C   LAB   word            Load Auto Byte ("auto" works with parameters/local variables) */
+#define OP_LAW  0x2D    /* 2D   LAW   word            Load Auto Word    */
+#define OP_LAD  0x2E    /* 2E   LAD   word            Load Auto Dword   */
+#define OP_SAB  0x2F    /* 2F   SAB   word            Store Auto Byte   */
+#define OP_SAW  0x30    /* 30   SAW   word            Store Auto Word   */
+#define OP_SAD  0x31    /* 31   SAD   word            Store Auto Dword  */
+#define OP_LABA 0x32    /* 32   LABA  word            Load Auto Byte Array      */
+#define OP_LAWA 0x33    /* 33   LAWA  word            Load Auto Word Array      */
+#define OP_LADA 0x34    /* 34   LADA  word            Load Auto Dword Array     */
+#define OP_SABA 0x35    /* 35   SABA  word            Store Auto Byte Array     */
+#define OP_SAWA 0x36    /* 36   SAWA  word            Store Auto Word Array     */
+#define OP_SADA 0x37    /* 37   SADA  word            Store Auto Dword Array    */
+#define OP_LEAA 0x38    /* 38   LEAA  word            Load Effective Auto Address   */
+#define OP_LSB  0x39    /* 39   LSB   word            Load Static Byte  */
+#define OP_LSW  0x3A    /* 3A   LSW   word            Load Static Word  */
+#define OP_LSD  0x3B    /* 3B   LSD   word            Load Static Dword */
+#define OP_SSB  0x3C    /* 3C   SSB   word            Store Static Byte */
+#define OP_SSW  0x3D    /* 3D   SSW   word            Store Static Word */
+#define OP_SSD  0x3E    /* 3E   SSD   word            Store Static Dword        */
+#define OP_LSBA 0x3F    /* 3F   LSBA  word            Load Static Byte Array    */
+#define OP_LSWA 0x40    /* 40   LSWA  word            Load Static Word Array    */
+#define OP_LSDA 0x41    /* 41   LSDA  word            Load Static Dword Array   */
+#define OP_SSBA 0x42    /* 42   SSBA  word            Store Static Byte Array   */
+#define OP_SSWA 0x43    /* 43   SSWA  word            Store Static Word Array   */
+#define OP_SSDA 0x44    /* 44   SSDA  word            Store Static Dword Array  */
+#define OP_LESA 0x45    /* 45   LESA  word            Load Effective Static Address */
+#define OP_LXB  0x46    /* 46   LXB   word            Load eXtern Byte  */
+#define OP_LXW  0x47    /* 47   LXW   word            Load eXtern Word  */
+#define OP_LXD  0x48    /* 48   LXD   word            Load eXtern Dword */
+#define OP_SXB  0x49    /* 49   SXB   word            Store eXtern Byte */
+#define OP_SXW  0x4A    /* 4A   SXW   word            Store eXtern Word */
+#define OP_SXD  0x4B    /* 4B   SXD   word            Store eXtern Dword        */
+#define OP_LXBA 0x4C    /* 4C   LXBA  word            Load eXtern Byte Array    */
+#define OP_LXWA 0x4D    /* 4D   LXWA  word            Load eXtern Word Array    */
+#define OP_LXDA 0x4E    /* 4E   LXDA  word            Load eXtern Dword Array   */
+#define OP_SXBA 0x4F    /* 4F   SXBA  word            Store eXtern Byte Array   */
+#define OP_SXWA 0x50    /* 50   SXWA  word            Store eXtern Word Array   */
+#define OP_SXDA 0x51    /* 51   SXDA  word            Store eXtern Dword Array  */
+#define OP_LEXA 0x52    /* 52   LEXA  word            Load Effective eXtern Address */
+#define OP_SXAS 0x53    /* 53   SXAS  -               Set eXtern Array Source   */
+#define OP_LECA 0x54    /* 54   LECA  word            Load Effective Code Address  (used to address direct string in bytecode)  */
+#define OP_SOLE 0x55    /* 55   SOLE  -               Selector for Object List Entry    */
+#define OP_END  0x56    /* 56   END   -               END of handler (end of handler, used also as return)  */
+#define OP_BRK  0x57    /* 57   BRK   -               BReaKpoint for debugging  */
+
+#if defined(__GNUC__)
+#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#elif defined(_MSC_VER )
+#define PACK(__Declaration__) __pragma(pack(push,1)) __Declaration__ __pragma(pack(pop))
+#else
+#error "Unknown platform!"
+#endif
+
+
+PACK(struct SOPScriptHeader
+{
+  uint16_t static_size;  // probably size of class variables/constants (??)
+  uint32_t import_resource; // the number of the corresponding import resource
+  uint32_t export_resource; // the number of the corresponding export resource
+  uint32_t parent; // the number of parent object (ffffffff if none)
+});
+
+
+class Aesop {
+    std::vector<uint8_t> sop_data;
+    SOPScriptHeader sopHeader;
+    uint16_t local_var_size;
+    uint32_t position;
+
+private:
+    uint8_t &getByte();
+    uint16_t &getWord();
+    uint32_t &getLong();
+
+public:
+    Aesop(std::vector<uint8_t> &data);
+    virtual ~Aesop();
+    void show();
+};
+}
+
+#endif /* AESOP_HPP */
